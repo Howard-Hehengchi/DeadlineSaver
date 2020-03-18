68f06a1ddc819944b4606f470ca798544f67c779
Howard <1214525153@qq.com>
2020-03-18 20:16:08 +0800

    First Commit

---
+++ app/src/main/java/com/deadlinesaver/android/MainActivity.java
@@ -0,0 +1,15 @@
+package com.deadlinesaver.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+https://github.com/Howard-Hehengchi/DeadlineSaver.git
